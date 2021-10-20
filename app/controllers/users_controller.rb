40a46a553ed74740c9ede9d92524a0ca78d5edfb
iknm0611 <iknm0611@gmail.com>
2021-10-20 11:05:43 +0000

    [Fix] credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,33 @@
 class UsersController < ApplicationController
-  
-  
-  
+
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def index
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @posts = Post.all
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
+  end
+
 end
+
+
